2c2402a505fd7c8eaca15d4c52dfc293fc1ac92e
François Proulx <francois@boostsecurity.io>
2025-09-21 10:41:14 +0200

    Changed port

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def new_poem():
     return render_template('index.html', poem=poem)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
